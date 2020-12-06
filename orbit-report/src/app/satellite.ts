7e4b34054d88287aba45fadd150c8acebc80f9eb
j6512 <justins8287@gmail.com>
2020-12-05 18:01:31 -0600

    Finished Task 6: Highlight Space Debris

--- orbit-report/src/app/satellite.ts
+++ orbit-report/src/app/satellite.ts
@@ -12,4 +12,12 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning(): boolean {
+        if (this.type === 'Space Debris') {
+            return true;
+        }
+
+        return false;
+    }
 }
